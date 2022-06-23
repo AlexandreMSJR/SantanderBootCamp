9f196b406909a9e9153dc640efa7e9be2c325910
Alexandre Medeiros dos Santos Junior <alexandremedeirosjunior@gmail.com>
2022-06-23 16:13:49 -0300

    Import e Export

---
+++ Modulo II (HTML5, CSS3 e JS)/JavaScript/Módulo/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs"
+
+console.log(mostraIdade("Alexandre", 24))
+console.log(mostraCidade("Alexandre", "Curitiba"))
+console.log(mostraHobby("Alexandre", "jogar video-Games"))
+console.log(mostraIdade("Pedro", 22))

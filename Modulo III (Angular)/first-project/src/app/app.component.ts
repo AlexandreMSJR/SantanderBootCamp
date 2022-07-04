3fd9ed7f2f20160e68dc23cffa9f922b7a1ba9cd
Alexandre Medeiros dos Santos Junior <alexandremedeirosjunior@gmail.com>
2022-07-04 17:34:25 -0300

    Diretivas

--- Modulo III (Angular)/first-project/src/app/app.component.ts
+++ Modulo III (Angular)/first-project/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first-project';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Ivonaldo Junior Escobar Tavares'
+  text: string = ''
+
+  pessoas = [
+    {
+      nome: 'Ivonaldo',
+      sobrenome: 'Soares'
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silva'
+    },
+    {
+      nome: 'João',
+      sobrenome: 'Pereira'
+    },
+
+    {
+      nome: 'Marcio',
+      sobrenome: 'Santos'
+    }
+  ]
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.pessoas)
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count == 10) {
+        clearInterval(interval)
+      }
+    }, 1000)
+
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em min', nome)
+  }
+
 }

b0ad3141135ae3a8ee8ee7f564c43064f2a8d3a9
Alexandre Medeiros dos Santos Junior <alexandremedeirosjunior@gmail.com>
2022-07-14 17:11:43 -0300

    Dio Certificado - 14.07.2022

---
+++ Módulo IV (Java)/Dio-test-java/PrimeiroProjeto/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá mundo");
+        
+        int i = 2;
+        int I;
+        // int i;
+        // int 1a;
+        int _1a;
+        int $aq;
+
+        final int j = 10; // Quando aplicamos FINAL, significa que ele é constante.
+        // j = 15;
+        int asrn24678md;
+        // int asrn246 78md;
+        int asrn2$4678_md;
+        // int asrn2$4%678_md;
+
+        final int NUMERO_TENTATIVAS = 5;
+        // final int numeroTentativas = 5;
+
+        String st1 = "Fulano";
+        String st2 = "Ciclano";
+        String st3 = "ag jaifdjasoif 214821";
+
+        System.out.println(i);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+
+        
+    }
+}

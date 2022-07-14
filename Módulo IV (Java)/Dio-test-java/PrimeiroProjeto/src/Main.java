25d2d836768076d2daf2a7450cc1aa180f5a5d61
Alexandre Medeiros dos Santos Junior <alexandremedeirosjunior@gmail.com>
2022-07-14 18:16:48 -0300

    Execícios de criação JAVA

---
+++ Módulo IV (Java)/Dio-test-java/PrimeiroProjeto/src/Main.java
@@ -0,0 +1,25 @@
+public class main {
+    
+    public static void main(String[] args) {
+
+        // Calculadora
+        System.out.println("Exercício calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        // Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.mensagem(4);
+        Mensagem.mensagem(12);
+        Mensagem.mensagem(23);
+
+        // Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+    }
+}
